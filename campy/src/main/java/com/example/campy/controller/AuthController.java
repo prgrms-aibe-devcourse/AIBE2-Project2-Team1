b77b12170d8e506d4affafbfc99b0c3cea536541
slowtree999 <zxcv828133@gmail.com>
2025-07-18 17:19:00 +0900

    로그인 #2

---
+++ campy/src/main/java/com/example/campy/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.campy.controller;
+
+import com.example.campy.dto.LoginForm;
+import com.example.campy.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid LoginForm form) {
+        String token = authService.login(form);
+        return ResponseEntity.ok().body(token);
+    }
+}
