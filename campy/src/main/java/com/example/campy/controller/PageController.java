450a8774951b61b50cf9a8d5c694b6d6acaf66e3
Ssun-shine <hayonini@gmail.com>
2025-07-21 16:17:31 +0900

    feat/#6 메인페이지 확인

--- campy/src/main/java/com/example/campy/controller/PageController.java
+++ campy/src/main/java/com/example/campy/controller/PageController.java
@@ -1,13 +1,19 @@
 package com.example.campy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    @GetMapping("/")
+    public String mainPage() {
+        return "main.html";
+    }
 
-    @GetMapping("/login")
+    @GetMapping("auth/login")
     public String loginPage() {
         return "login"; // → templates/login.html 렌더링
     }
+
 }
