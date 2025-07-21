4eb45f768c3d2d18463928c3d52496ff060a84db
ahyunkkk <a_hyunii@naver.com>
2025-07-21 12:22:16 +0900

    자료 등록 기능 - 컨트롤러 수정 #4

--- campy/src/main/java/com/example/campy/controller/MaterialController.java
+++ campy/src/main/java/com/example/campy/controller/MaterialController.java
@@ -2,14 +2,14 @@ package com.example.campy.controller;
 
 import com.example.campy.dto.MaterialRequestDto;
 import com.example.campy.dto.MaterialResponseDto;
+import com.example.campy.entity.User;
+import com.example.campy.jwt.JwtUtil;
+import com.example.campy.repository.UserRepository;
 import com.example.campy.service.MaterialService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,10 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class MaterialController {
 
     private final MaterialService materialService;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
     @PostMapping
-    public ResponseEntity<MaterialResponseDto> createMaterial(@RequestBody MaterialRequestDto requestDto) {
-        Integer sellerId = 1;
+    public ResponseEntity<MaterialResponseDto> createMaterial(
+            @RequestHeader("Authorization") String authHeader,
+            @RequestBody MaterialRequestDto requestDto) {
+
+        // 1. Bearer 제거
+        String token = authHeader.replace("Bearer ", "");
+
+        // 2. 토큰에서 이메일 꺼냄
+        String email = jwtUtil.getEmail(token);
+
+        // 3. 이메일로 사용자 조회
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("사용자 정보를 찾을 수 없습니다."));
+
+        // 4. 해당 사용자의 id를 sellerId로 사용
+        Integer sellerId = user.getUserId();
+
         MaterialResponseDto responseDto = materialService.createMaterial(requestDto, sellerId);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
